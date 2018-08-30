fde62ffc5cf263c6d19177da6faf4a0749c4c8d3
teggot <fergon4ik@gmail.com>
2018-08-30 19:30:01 +0300

    filter fixes

--- core/views.py
+++ core/views.py
@@ -15,23 +15,36 @@ class HomeView(TemplateView):
         context['tags'] = Tag.objects.all()
         return context
 
+    def dispatch(self, request, *args, **kwargs):
+        print(request.method)
+        if request.method == "POST":
+            return self.post(request, *args, **kwargs)
+        elif request.method == "GET":
+            return self.get(request, )
+
     def get(self, request, *args, **kwargs):
+        context = self.get_context_data(**kwargs)
+        context['products'] = Product.objects.all()
+
+        return self.render_to_response(context)
+
+    def post(self, request, *args, **kwargs):
         context = self.get_context_data(**kwargs)
         query = Product.objects.all()
         try:
-            price_min = request.GET['price_min']
+            price_min = request.POST['price_min']
             context['price_min'] = price_min
             query = query.filter(price__gte=price_min)
         except:
             pass
         try:
-            price_max = request.GET['price_max']
+            price_max = request.POST['price_max']
             context['price_max'] = price_max
             query = query.filter(price__lte=price_max)
         except:
             pass
         try:
-            tags = request.GET.getlist("tags")
+            tags = request.POST.getlist("tags")
             context['checked_tags'] = tags
             print('tags =', tags)
             if len(tags) != 0:
